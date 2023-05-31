de0eba90624c26b9a21ba6cb8129eb16f0447320
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-05-31 15:23:47 +0300

    Refactoring

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -22,7 +22,7 @@ const ContactList = ({ contacts, filter, handleDeleteContact }) => {
 
 ContactList.propTypes = {
   contacts: PropTypes.arrayOf(
-    PropTypes.exact({
+    PropTypes.shape({
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       number: PropTypes.string.isRequired,
